4b284c82679c3e76e3d72b5b06d1ea99f63ca7a1
David <108626542+deivih84@users.noreply.github.com>
2023-11-29 18:33:11 +0100

    awa

--- X2_UlloaDeLaCalle.c
+++ X2_UlloaDeLaCalle.c
@@ -5,23 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <sys/wait.h>
 #include <unistd.h>
 #include <ctype.h>
-//#include <pthread.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 #define MAX_COMMAND_LENGTH 800
 #define MAX_ARGUMENTS 5
-
-// Estructura para pasar datos al hilo del proveedor
-typedef struct {
-    char *ruta;
-    char *fichDestino;
-    int T;
-    int P;
-    int C;
-} ProveedorData;
 
 typedef struct {
     char tipo;
@@ -33,16 +24,38 @@ typedef struct {
     int total['j' - 'a' + 1];
 } TotalProductos;
 
+// Estructura para compartir datos entre proveedor y consumidor
+typedef struct {
+    sem_t sem_proveedor;  // Semáforo para la exclusión mutua del buffer entre proveedor y consumidor
+    sem_t sem_consumidor; // Semáforo para la exclusión mutua del buffer entre consumidor y proveedor
+    char *ruta;
+    char *fichDestino;
+    int T;
+    int P;
+    int C;
+    int in;               // Índice de escritura en el búfer
+    int out;              // Índice de lectura en el búfer
+    Producto *buffer;     // Búfer circular
+    TotalProductos totalProductos; // Registro del total de productos
+} SharedData;
+
+// Variable de condición para la sincronización entre proveedor y consumidor
+pthread_cond_t cond_proveedor = PTHREAD_COND_INITIALIZER;
+
 void proveedorFunc(void *data);
+
+void consumidorFunc(void *data);
+
 bool esTipoValido(char c);
+
 bool esCadena(char *string);
 
-char path[MAX_COMMAND_LENGTH];
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     char *path = argv[1];
     int arg3 = atoi(argv[3]), arg4 = atoi(argv[4]), arg5 = atoi(argv[5]);
-
+    SharedData sharedData;
+    FILE *file;
 
 
     // Verificación de la cantidad de argumentos
@@ -52,58 +65,73 @@ int main (int argc, char *argv[]) {
     }
 
     //Verificación parámetros
-    if (esCadena(argv[3]) || arg3 <= 0 || arg3 > 5000){
-         fprintf(stderr, "Error: T debe ser un entero positivo menor o igual a 5000.\n");
+    if (esCadena(argv[3]) || arg3 <= 0 || arg3 > 5000) {
+        fprintf(stderr, "Error: T debe ser un entero positivo menor o igual a 5000.\n");
         return -1;
     }
-    if (esCadena(argv[4]) || arg4 <= 0 || arg4 > 7){
+    if (esCadena(argv[4]) || arg4 <= 0 || arg4 > 7) {
         fprintf(stderr, "Error: P debe ser un entero positivo menor o igual a 7.\n");
         return -1;
     }
-    if (esCadena(argv[5]) || arg5 <= 0 || arg5 > 1000){
+    if (esCadena(argv[5]) || arg5 <= 0 || arg5 > 1000) {
         fprintf(stderr, "Error: C debe ser un entero positivo menor o igual a 1000.\n");
         return -1;
     }
     sprintf(path, "%s\\proveedor%d.dat", argv[1], 0);
-    printf("%s\n", path);
+    file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error al abrir el archivo de entrada del proveedor %d.\n", sharedData.P);
+    }
 
-    ProveedorData sharedData;
-    sharedData.ruta = argv[1]; // Ruta de los archivos de entrada.
+
+    sharedData.ruta = path; // Ruta de los archivos de entrada
     sharedData.fichDestino = argv[2]; // Nombre del fichero destino.
     sharedData.T = arg3; // Tamaño del búfer circular.
     sharedData.P = arg4; // Número total de proveedores.
     sharedData.C = arg5; // Número total de clientes.
 
-    proveedorFunc(&sharedData);
+    // Crear estructuras de datos compartidas
+    sharedData.in = 0;
+    sharedData.out = 0;
+    sharedData.buffer = (Producto *) malloc(sharedData.T * sizeof(Producto));
+    if (sharedData.buffer == NULL) {
+        fprintf(stderr, "Error al asignar memoria para el búfer compartido.\n");
+        return -1;
+    }
+    sem_init(&sharedData.sem_proveedor, 0, 1);
+    //sem_init(&sharedData.sem_consumidor, 0, 1);
+
+    // Crear hilo del proveedor
+    pthread_t proveedorThread;
+    pthread_create(&proveedorThread, NULL, (void *) proveedorFunc, &sharedData);
+
+    // Crear hilo del consumidor
+    pthread_t consumidorThread;
+    //pthread_create(&consumidorThread, NULL, (void *) consumidorFunc, &sharedData);
+
+    // Esperar a que los hilos terminen
+    pthread_join(proveedorThread, NULL);
+    //pthread_join(consumidorThread, NULL);
+
+    // Destruir semáforos y liberar memoria
+    sem_destroy(&sharedData.sem_proveedor);
+    //sem_destroy(&sharedData.sem_consumidor);
+    free(sharedData.buffer);
 
     return 0;
 }
 
 void proveedorFunc(void *data) {
-    ProveedorData *proveedor_data = (ProveedorData *)data;
+    SharedData *sharedData = (SharedData *) data;
     FILE *file, *outputFile;
-    Producto *buffer;
-    int in = 0, out = 0; // Índice de escritura y lectura en el búfer
     char c;
     int productosLeidos = 0, productosValidos = 0, productosInvalidos = 0;
-    TotalProductos totalProductos = {0};
+    TotalProductos totalProductos = {{0}};
 
     // Abrir el archivo de entrada del proveedor
-    file = fopen(proveedor_data->ruta, "r");
+    //sprintf(sharedData->ruta, "%sproveedor%d.dat", sharedData->ruta, sharedData->P);
 
-    if (file == NULL) {
-        fprintf(stderr, "Error al abrir el archivo de entrada del proveedor %d.\n", proveedor_data->P);
-    }
-
-    // Inicializar el búfer circular
-    buffer = (Producto *)malloc(proveedor_data->T * sizeof(Producto));
-
-    if (buffer == NULL) {
-        fprintf(stderr, "Error al asignar memoria para el búfer del proveedor %d.\n", proveedor_data->P);
-        fclose(file); // Cierra el archivo antes de salir
-        return;  // Agrega un return para salir de la función en caso de error
-    }
-
+    file = fopen(sharedData->ruta, "r"); ///////////
 
     // Leer y procesar productos del archivo
     while ((c = fgetc(file)) != EOF) {
@@ -111,11 +139,18 @@ void proveedorFunc(void *data) {
 
         if (esTipoValido(c)) {
             // Procesar productos válidos
-            Producto nuevoProducto = {c, proveedor_data->P}; //Variable en medio del código
-            buffer[in] = nuevoProducto;
-            in = (in + 1) % proveedor_data->T;
+            // Incluir semáforo de exclusión mutua para la escritura en el búfer
+            sem_wait(&sharedData->sem_proveedor);
+            // Escribir en el búfer
+            sharedData->buffer[sharedData->in].tipo = c;
+            sharedData->buffer[sharedData->in].proveedorID = sharedData->P;
+            sharedData->in = (sharedData->in + 1) % sharedData->T;
+            // Incrementar contador de productos válidos
             productosValidos++;
+            // Actualizar registro de productos
             totalProductos.total[c - 'a']++;
+            // Liberar semáforo de exclusión mutua
+            sem_post(&sharedData->sem_proveedor);
         } else {
             // Procesar productos inválidos
             productosInvalidos++;
@@ -124,15 +159,14 @@ void proveedorFunc(void *data) {
     fclose(file); // Cerrar el archivo
 
     // Escribir resultados en el archivo de salida
-
-    outputFile = fopen(proveedor_data->fichDestino, "w");
+    outputFile = fopen(sharedData->fichDestino, "w");
     if (outputFile == NULL) {
-        fprintf(stderr, "Error al abrir el archivo de salida del proveedor %d.\n", proveedor_data->P);
-        free(buffer);
-      return;
+        fprintf(stderr, "Error al abrir el archivo de salida del proveedor %d.\n", sharedData->P);
+        free(sharedData->buffer);
+        return;
     }
 
-    fprintf(outputFile, "Proveedor: %d\n", proveedor_data->P);
+    fprintf(outputFile, "Proveedor: %d\n", sharedData->P);
     fprintf(outputFile, "Productos Inválidos: %d\n", productosInvalidos);
     fprintf(outputFile, "Productos Válidos: %d. De los cuales se han insertado:\n", productosValidos);
 
@@ -143,10 +177,42 @@ void proveedorFunc(void *data) {
     // Cerrar el archivo de salida
     fclose(outputFile);
 
-    printf("Debug dentro de proveedorFunc");
+    // Avisar al consumidor que ha terminado
+    pthread_cond_signal(&cond_proveedor);
+}
 
-    // Liberar memoria del búfer
-    free(buffer);
+void consumidorFunc(void *data) {
+    SharedData *sharedData = (SharedData *) data;
+    FILE *file, *outputFile;
+    Producto productoConsumido;
+    int productosConsumidos = 0;
+    int totalProductosEsperados = sharedData->T * sharedData->C;
+    int productosConsumidosPorTipo['j' - 'a' + 1] = {0};
+    int productosConsumidosPorProveedor[sharedData->P];
+
+    // Incluir semáforo de exclusión mutua para la lectura del búfer
+    // Esperar a que el proveedor haya terminado de producir
+    pthread_cond_wait(&cond_proveedor, &sharedData->sem_consumidor);
+
+    // Leer del búfer
+    productoConsumido = sharedData->buffer[sharedData->out];
+    sharedData->out = (sharedData->out + 1) % sharedData->T;
+
+    // Incrementar contador de productos consumidos
+    productosConsumidos++;
+
+    // Actualizar registro de productos consumidos
+    productosConsumidosPorTipo[productoConsumido.tipo - 'a']++;
+    productosConsumidosPorProveedor[productoConsumido.proveedorID]++;
+
+    // Liberar semáforo de exclusión mutua
+    sem_post(&sharedData->sem_consumidor);
+
+    // Indicar que ha terminado de consumir
+    pthread_cond_signal(&cond_proveedor);
+
+    // Salir de la función del consumidor
+    pthread_exit(NULL);
 }
 
 bool esTipoValido(char c) {
